130b40b59e3c432c8a31544348ec0618375970fd
Reginald Robinson <ddrbnsn@gmail.com>
2024-01-04 00:40:00 -0800

    readme ideas and ledger

---
+++ ledger.js
@@ -0,0 +1,112 @@
+let ledgerBasic = {
+	"tx1":{
+		"sender":"p1ID",
+		"receiber":"p2ID",
+		"amount":4
+	},
+
+	"tx2":{
+		"sender":"p1ID",
+		"receiber":"p2ID",
+		"amount":4
+	},
+
+	"tx3":{
+		"sender":"p1ID",
+		"receiber":"p2ID",
+		"amount":4
+	},
+
+	"tx4":{
+		"sender":"p1ID",
+		"receiber":"p2ID",
+		"amount":4
+	},
+	"tx5":{
+		"sender":"p1ID",
+		"receiber":"p2ID",
+		"amount":4
+	},
+
+	"tx6":{
+		"sender":"p1ID",
+		"receiber":"p2ID",
+		"amount":4
+	},
+	"tx7":{
+		"sender":"p1ID",
+		"receiber":"p2ID",
+		"amount":4
+	},
+
+	"tx8":{
+		"sender":"p1ID",
+		"receiber":"p2ID",
+		"amount":4
+	},
+	"tx9":{
+		"sender":"p1ID",
+		"receiber":"p2ID",
+		"amount":4
+	},
+
+	"tx10":{
+		"sender":"p1ID",
+		"receiber":"p2ID",
+		"amount":4
+	},
+}
+
+let ledgerSafe ={
+
+	"Block1":{
+		"prevHash":"00000",
+		"tx":{
+			"sender":"p1ID",
+			"receiber":"p2ID",
+			"amount":4
+		},
+		"chainHash":"00100"
+	},
+	"Block2":{
+		"prevHash":"00100",
+		"tx":{
+			"sender":"p1ID",
+			"receiber":"p2ID",
+			"amount":4
+		},
+		"chainHash":"23000"
+	},
+
+	"Block3":{
+		"prevHash":"23000",
+		"tx":{
+			"sender":"p1ID",
+			"receiber":"p2ID",
+			"amount":4
+		},
+		"chainHash":"45869"
+	},
+
+	"Block4":{
+		"prevHash":"45869",
+		"tx":{
+			"sender":"p1ID",
+			"receiber":"p2ID",
+			"amount":4
+		},
+		"chainHash":"76543"
+	},
+}
+
+
+/**
+ * ledger with multiple txs per block
+ * proof mechanism to guarantee consensus
+ * attack resistant
+ * 
+ *
+ *
+ *
+ *
+ * **/

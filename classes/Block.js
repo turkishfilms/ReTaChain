667c6c6ac2466efab06cac605d79b8d5b636e155
Reginald Robinson <ddrbnsn@gmail.com>
2024-01-04 00:38:30 -0800

    Ideas ledger and readme

--- classes/Block.js
+++ classes/Block.js
@@ -4,7 +4,12 @@
 
  * **/
 class Block{
-constructor({}={}){
+constructor({previousHash,transactions = [],date}={}){
+		this.previousHash = previousHash
+		this.transactions = transactions
+		this.date = date
+		this.chainHash = hash(previousHash,transactions, date)
+		this.nonce = 0
 }
 
 }

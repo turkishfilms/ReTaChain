4caa472adc90ed9203644a1c3bc88e572898ac72
taborgreat <taborgreat@gmail.com>
2024-01-05 15:44:00 -0800

    added basic hashing and proof of work

--- classes/Blockchain.js
+++ classes/Blockchain.js
@@ -1,3 +1,6 @@
+import sha256 from 'sha256';
+
+
 /**
  @class
  @param
@@ -8,14 +11,16 @@
 	constructor() {
 	  this.chain = [];
 	  this.pendingTransactions = [];
+
+	  this.createNewBlock(100, '0', '0'); // genesis block
 	}
   
-	createNewBlock(previousBlockHash, hash) {
+	createNewBlock(nonce, previousBlockHash, hash) {
 	  const newBlock = {
 		index: this.chain.length + 1,
 		timestamp: Date.now(),
 		transactions: this.pendingTransactions,
-		nonce: 0, //fill in once proof of... is established
+		nonce: nonce, //fill in once proof of... is established
 		hash: hash,
 		previousBlockHash: previousBlockHash,
 	  };
@@ -39,6 +44,24 @@
 	  this.pendingTransactions.push(newTransaction);
   
 	  return this.getLastBlock()['index'] + 1;
+	}
+
+	hashBlock(previousBlockHash, currentBlockData, nonce){
+		const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+		const hash = sha256(dataAsString);
+		return hash;
+	}
+
+	proofOfWork(previousBlockHash, currentBlockData){
+		let nonce = 0;
+		let hash = this.hashBlock(previousBlockHash,currentBlockData,nonce);
+		while (hash.substring(0,4) !== '0000') {
+			nonce++;
+			hash = this.hashBlock(previousBlockHash,currentBlockData,nonce);
+			console.log(hash);
+		}
+		console.log("block returned with '0000' at: " + nonce + " nonce")
+		return nonce;
 	}
   }
   
